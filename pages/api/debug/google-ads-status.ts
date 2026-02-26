ade691fa8b08be73e7ddbf19e328230ac6e705e8
Antonio Mendoza <antonio@siteoptz.ai>
2026-02-25 18:20:53 -0600

    Fix import path in Google Ads status debug endpoint

--- pages/api/debug/google-ads-status.ts
+++ pages/api/debug/google-ads-status.ts
@@ -2,7 +2,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../auth/[...nextauth]';
-import { getValidAccessToken } from '../../lib/google-token-utils';
+import { getValidAccessToken } from '../../../lib/google-token-utils';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
